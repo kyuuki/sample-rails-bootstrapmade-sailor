5a004a1d9b926f63cb87bbebd2d117c9e9b4da17
kyuuki <kyuuki.japan@gmail.com>
2021-08-30 08:44:44 +0000

    トップページ追加

---
+++ app/controllers/static_page_controller.rb
@@ -0,0 +1,5 @@
+class StaticPageController < ApplicationController
+  def top
+    # render :top  # これが省略されている
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_page#top'
+  # root to: 'static_page#top'  # 上記はこれの省略形
 end
